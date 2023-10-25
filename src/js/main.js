f29dedf9b9a37e3993b0c2acdb45de03b7136f90
Maria Arkhipova <marias1403@yandex.ru>
2023-10-25 16:58:46 +0900

    feat: layout and styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import '../assets/styles/index.css';
+
+const selectBox = document.querySelector('.custom-select .select-box');
+const options = document.querySelectorAll('.custom-select .options .option');
+const hiddenSelect = document.querySelector('.custom-select .hidden-select');
+
+selectBox.addEventListener('click', () => {
+  selectBox.classList.toggle('active');
+});
+
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    selectBox.querySelector('.selected-option').textContent = option.textContent;
+    hiddenSelect.value = option.getAttribute('data-value');
+    selectBox.classList.remove('active');
+  });
+});
